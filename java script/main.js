68665f20410aa51a7d7618f7fbced4738fbad533
aminelaarif <laarifamine5569@gmail.com>
2024-01-12 15:38:47 +0100

    changed style completely optimized code by replacing the buttons with just 2 inputs and 2 buttons

--- java script/main.js
+++ java script/main.js
@@ -8,6 +8,7 @@ function createRestaurent (name,dailyTables,dailyPlates) {
         reserve:reserve,
         checkout:checkout,
         newday:newday,
+        displaycont:displaycont,
         income: 0
     }
 
@@ -20,7 +21,7 @@ function createRestaurent (name,dailyTables,dailyPlates) {
 
 var reserve = function(){
     if  (this.remainingPlates===0 ){
-      return   $(".center").html("we don't have enoough paltes let's start a new day ") 
+      return   $(".center").html("we don't have enough paltes let's start a new day ") 
     }
     else if (this.availableTables===0){
          return $(".center").html("we don't have enough tables don't accept any reservation until one is available ")
@@ -43,23 +44,47 @@ var checkout = function(){
   
 }
 
+// displays the remaining tables  and remaining plates 
+var displaycont = function (){
+    console.log(this)
+    $(".analyse").html(`you have ${this.remainingPlates}  plates left  and   ${this.availableTables} tables left  `)
+}
+ // resets everything and it show's the income 
 var newday = function(){
     this.availableTables=this.dailyTables
     this.remainingPlates=this.dailyPlates
-    $("html").html(`<h3 class='closed' >we are closed here is your income ${Math.floor(this.income)} $ </h3>` )
+    $("body").html(`<h3 class='closed' id="clo" >we are closed here is your income ${Math.floor(this.income)} $ </h3>` )
     this.income = 0
 }
 
-var resto1 = createRestaurent("bonjorno",8,16)
+var resto1 = createRestaurent("bonjorno",8,16)  // create our instance 
 
 
 
 
-$(".plates").on("click", function(event){
-    $(".analyse").html(`you have ${this.dailyPlates}  plates left  and   ${this.availableTables} tables left  `)
-})
+///  grabs the input   and it should be .table1 until .table8  adds a picture to  mark it as reserved 
+
+$("#grabres").on("click",function(){
+    tableNumber = $('#resinput').val()
+    $(`${tableNumber}`).html("<img class='croped' src='https://www.ukpos.com/media/catalog/product/r/s/rspsitu.png' >")
+    $(".announcer").html(`${tableNumber} is reserved`)
+
+    resto1.reserve()
+    
+    
+    console.log(tableNumber)
+ })
 
 
+ ///  grabs the input   and it should be .table1 until .table8  to checkout and make the table available to be reserved 
+ $("#grabchec").on("click",function(){
+    checkoutNumber = $('#checinput').val()
+    $(`${checkoutNumber}`).html("")
+    $(".announcer").html(`${checkoutNumber} is available`)
+    resto1.checkout()
+    
+    console.log(checkoutNumber)
+ })
 
 
 
@@ -70,80 +95,7 @@ $(".plates").on("click", function(event){
 
 
 
-$(".table1").on("click",function(event){
-    $(".announcer").html("Table1 is reserved")
-    $(".center").html("") 
-    $(".box1").html("<img class='croped' src='/images/Fish_plate.jpeg' >")
-})
-$(".table2").on("click",function(event){
-    $(".announcer").html("Table2 is reserved")
-    $(".box2").html("<img class='croped' src='/images/meat.jpeg' >")
-})
-$(".table3").on("click",function(event){
-    $(".announcer").html("Table3 is reserved")
-    $(".box3").html("<img class='croped' src='/images/pizza.jpeg' >")
-})
-$(".table4").on("click",function(event){
-    $(".announcer").html("Table4 is reserved")
-    $(".box4").html("<img class='croped' src='/images/puttanesca.jpg' >")
-})
-$(".table5").on("click",function(event){
-    $(".announcer").html("Table5 is reserved")  
-    $(".box5").html("<img class='croped' src='/images/sweets.jpg' >")                      /// to annouce reservation of a table 
-})
-$(".table6").on("click",function(event){
-    $(".announcer").html("Table6 is reserved")
-    $(".box6").html("<img class='croped' src='/images/pizza.jpeg' >")
-})
-$(".table7").on("click",function(event){
-    $(".announcer").html("Table7 is reserved")
-    $(".box7").html("<img class='croped' src='/images/pizza.jpeg' >")
-})
-$(".table8").on("click",function(event){
-    $(".announcer").html("Table8 is reserved")
-    $(".box8").html("<img class='croped' src='/images/pizza.jpeg' >")
 
-})
-  
-
-
-
-
-
-
-
-$(".ctable1").on("click",function(event){
-    $(".announcer").html("Table1 is available")
-    $(".box1").html("")
-})
-$(".ctable2").on("click",function(event){
-    $(".announcer").html("Table2 is available")
-    $(".box2").html("")
-})
-$(".ctable3").on("click",function(event){
-    $(".announcer").html("Table3 is available")
-    $(".box3").html("")
-})
-$(".ctable4").on("click",function(event){
-    $(".announcer").html("Table4 is available") 
-    $(".box4").html("")                                                      // to announce  the avability of a table 
-})
-$(".ctable5").on("click",function(event){
-    $(".announcer").html("Table5 is available")
-    $(".box5").html("")
-})
-$(".ctable6").on("click",function(event){
-    $(".announcer").html("Table6 is available")
-    $(".box6").html("")
-})
-$(".ctable7").on("click",function(event){
-    $(".announcer").html("Table7 is available")
-    $(".box7").html("")
-})
-$(".ctable8").on("click",function(event){
-    $(".announcer").html("Table8 is available")
-    $(".box8").html("")
-})
 
 
 
